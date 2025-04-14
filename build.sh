98f74e5d75a55b8171644931967a19e4fa4b0c3b
Crazycoders283 <crazycoders283@gmail.com>
2025-04-15 03:20:45 +0530

    Fix php command not found in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Define PHP version to use
+export PHP_VERSION="8.2"
+
 # Install Composer dependencies
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+/opt/render/project/util/render-php.sh composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 # Install NPM dependencies
 npm ci
@@ -10,9 +13,9 @@ npm ci
 npm run build
 
 # Laravel optimizations
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+/opt/render/project/util/render-php.sh php artisan config:cache
+/opt/render/project/util/render-php.sh php artisan route:cache
+/opt/render/project/util/render-php.sh php artisan view:cache
 
 # Set permissions
 chmod -R 755 storage bootstrap/cache
